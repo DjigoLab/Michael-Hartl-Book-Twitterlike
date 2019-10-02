59b7f640d88148f89890834c0ed45bb26c3df4c5
Antonio Djigo <antonio@secret-source.eu>
2019-10-02 08:54:06 +0100

    5.5 Adding users controller routes and test

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   get 'static_pages/home'
+
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
-  get  '/signup',  to: 'static_pages#signup'
   get  '/login',   to: 'static_pages#login'
+
+  get  '/signup',  to: 'users#new'
 
 
 
