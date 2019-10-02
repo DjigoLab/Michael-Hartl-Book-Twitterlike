9b55d5c8ec85df86cfa402db468ff1dc0f65e19e
Antonio Djigo <antonio@secret-source.eu>
2019-10-02 11:41:47 +0100

    6.4.1 Password  validation and password digest

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,7 +7,12 @@ class UserTest < ActiveSupport::TestCase
 
 
   def setup
-    @user = User.new(name: "Example ", email: "user@example.com")
+    @user = User.new(
+      name: "Example ",
+      email: "user@example.com",
+      password: "foobar",
+      password_confirmation: "foobar"
+    )
   end
 
   test "should be valid" do
@@ -58,6 +63,17 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_not duplicate_user.valid?
   end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
 
 
 end
