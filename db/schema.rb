87bf059429dc8485aedb30c042885609cf3c87bc
Antonio Djigo <antonio@secret-source.eu>
2019-10-02 10:57:50 +0100

    6.2.5 User validation for lenght, uniquenes...

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191002080545) do
+ActiveRecord::Schema.define(version: 20191002095204) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
