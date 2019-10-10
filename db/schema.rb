fec547ae8828ca73d8e1975c09cdf61820b55f35
Antonio Djigo <antonio@secret-source.eu>
2019-10-10 12:03:28 +0100

    9.4 Advanced login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191002101531) do
+ActiveRecord::Schema.define(version: 20191010110112) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20191002101531) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
